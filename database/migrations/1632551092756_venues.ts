be63003938a66980a6a2f9983eee7d385e958d8a
Ali Maulana <rahman_0601@apps.ipb.ac.id>
2021-09-27 18:12:23 +0700

    make it better

---
+++ database/migrations/1632551092756_venues.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Venues extends BaseSchema {
+  protected tableName = 'venues'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name', 255).notNullable()
+      table.string('phone', 255).notNullable()
+      table.string('address', 255).notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+       table.timestamp('created_at', { useTz: true }).notNullable()
+       table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1632552553988_fields.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Fields extends BaseSchema {
+  protected tableName = 'fields'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name', 255).notNullable()
+      table.string('type', 255).notNullable()
+      table.string('venuesId', 255).notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+       table.timestamp('created_at', { useTz: true }).notNullable()
+       table.timestamp('updated_at', { useTz: true }).notNullable()
+      // table.timestamps(true, true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
